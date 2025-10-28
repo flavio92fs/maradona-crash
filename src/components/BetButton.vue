773f8733aeaf97df38e2fd59b548f42f0375f73c
Mevendinz <vinsdesu@gmail.com>
2025-10-28 15:01:00 +0100

    fix buttons

--- src/components/BetButton.vue
+++ src/components/BetButton.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex items-center btn-container rounded-lg w-full mx-2 p-2">
     <button
-      class="relative flex md:flex-col btn rounded-xl text-xl p-2 w-full text-white text-start items-center justify-between md:items-start"
+      class="relative flex md:flex-col btn rounded-xl text-xl p-2 w-full text-white text-start items-start justify-between"
       :disabled="!active"
       :class="!active ? 'button-70-disabled' : 'button-70'"
       @click.stop="console.log('bet')"
     >
-      <div class="flex md:flex-col">
+      <div class="flex flex-col">
         <p class="font-bold">BET</p>
-        <p class="font-bold ms-1.5 md:ms-0">{{ betValue }}€</p>
+        <p class="font-bold">{{ betValue }}€</p>
       </div>
 
       <button
-        class="md:absolute select-none right-0 top-0 md:me-1 md:mt-1 h-6 w-6 md:h-7 md:w-7 z-10"
+        class="absolute select-none right-0 top-0 me-1 mt-2 h-6 w-6 md:h-7 md:w-7 z-10"
         v-if="id != menuOpen"
         :disabled="!active"
         @click.stop="$emit('sub-click')"
